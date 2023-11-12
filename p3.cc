6b319d219f46da9d8828ae1acafee002ece8ff73
Azl11 <109969558+Azl11@users.noreply.github.com>
2023-11-12 04:22:47 +0100

    Update p3.cc

--- p3.cc
+++ p3.cc
@@ -6,25 +6,21 @@ using namespace std;
 void Age(vector<vector<string> >mat,vector<vector<string> >&met){
     //Preguntas para eliminar elementos de la matriz
     string Kid_Adult;
-    cout << "Kid or Adult? ";
+    cout << "Kid o Adult? ";
     cin >> Kid_Adult;
-    if (Kid_Adult == "Kids"){
+    if (Kid_Adult == "Kid"){
         //Solo deja las opciones con Adult
         for(int i=0; i<9222; ++i){
-            for(int j=0; j<7; ++j){
                 if(mat[i][3]=="Kids"){
-                    met[i][j]=mat[i][j];
-                }
+                    met.push_back(mat[i]);
             }
         }
     }
     else if(Kid_Adult == "Adult"){
         //Solo deja las opciones con Adult
         for(int i=0; i<9222; ++i){
-            for(int j=0; j<7; ++j){
                 if(mat[i][3]=="Adult"){
-                    met[i][j]=mat[i][j];
-                }
+                    met.push_back(mat[i]);
             }
         }
     }
@@ -33,25 +29,22 @@ void Age(vector<vector<string> >mat,vector<vector<string> >&met){
 void Sex(vector<vector<string> >mat,vector<vector<string> >&met){
     //Preguntas para eliminar elementos de la matriz
     string Women_Man;
-    cout << "Female or Man? ";
+    cout << "Women or Man? ";
     cin >> Women_Man;
     if (Women_Man == "Man"){
         //Solo deja las opciones con Adult
         for(int i=0; i<9222; ++i){
-            for(int j=0; j<7; ++j){
                 if(mat[i][2]=="Male"){
-                    met[i][j]=mat[i][j];
-                }
+                    met.push_back(mat[i]);
             }
         }
+        cout<<met.size();
     }
-    else if(Women_Man == "Female"){
+    else if(Women_Man == "Women"){
         //Solo deja las opciones con Adult
         for(int i=0; i<9222; ++i){
-            for(int j=0; j<7; ++j){
                 if(mat[i][2]=="Female"){
-                    met[i][j]=mat[i][j];
-                }
+                    met.push_back(mat[i]);
             }
         }
     }
@@ -62,9 +55,8 @@ int main(){
     string Comodin;
     ifstream Data("PRODUCTO_DATA_BUENO.txt");
     vector<vector<string> > mat(9222, vector<string>(7));
-    vector<vector<string> >sex(9222, vector<string>(7));
-    vector<vector<string> >age(9222, vector<string>(7));
-    vector<vector<string> >sortAgeSex(9222, vector<string>(7));
+    vector<vector<string> >sex;
+    vector<vector<string> >age;
     for(int i=0; i<9222; ++i){
         for (int g=0; g<7;++g){
             Data>>Comodin;
@@ -73,18 +65,7 @@ int main(){
         }
     }
     Sex(mat,sex);
-    Age(mat,age);
-    for(int i=0;i<9222; ++i){
-        for (int g=0; g<7;++g){
-            if(sex[i][g]==age[i][g]){
-                sortAgeSex[i][g]=sex[i][g];
-                cout<<sortAgeSex[i][g]<<" ";
-            }
-    }
-    cout<<endl;
-    //Fa una purga dels gustos de la persona
-}
-
+    Age(sex,age);
 }
 
 
